2b27923abb741c1d1509b6ea41b5a57957e597ed
SypherHub <agu06726@gmail.com>
2025-08-31 15:39:52 +0900

    Create syperhub.lua

---
+++ syperhub.lua
@@ -0,0 +1,19 @@
+repeat task.wait() until game.Players.LocalPlayer and game.Players.LocalPlayer.Character
+if not game:IsLoaded() then
+    game.Loaded:Wait()
+end
+
+print("Supported game!")
+local creator = game.CreatorId
+
+local games = {
+    [1848960] = "https://pandadevelopment.net/virtual/file/adc9290f33bda7b4"
+
+} 
+
+if games[creator] then 
+    print("Please wait, loading..")
+    loadstring(game:HttpGet(games[creator]))()
+else
+    warn("Unsupported game.")
+end
